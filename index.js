ae3daa153f3235f5e839351730f32a2470b99e0d
jhygt46 <infolifeoperacional@gmail.com>
2020-08-10 17:47:12 -0400

    aa

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var socket = require('socket.io-client')('http://35.226.249.89:80');
+socket.on('connect', function(){ console.log("CONNECT"); });
+socket.on('event', function(data){ console.log(data); });
+socket.on('disconnect', function(){ console.log("DISCONNECT"); });
+
+/*
 var io = require('socket.io-client');
 var socket = io.connect('http://35.226.249.89:80', { reconnect: true });
 
@@ -29,3 +35,4 @@ app.get('/busqueda', urlencodedParser, function(req, res){
 	console.log(req.body.id);
 
 });
+*/
